e9495fd4d226faeef0d5259ab7f422abbf2f18f7
Gary A. Newsome <garyanewsome@gmail.com>
2016-11-26 22:31:19 -0500

    New brand new look

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -10,4 +10,7 @@ $(document).ready(function(){
       contactForm.reset();
     })
 
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal').modal();
+
 })
