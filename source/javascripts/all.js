32958e4755310436654671736cbdecccae732fbb
Gary A. Newsome <garyanewsome@gmail.com>
2017-01-03 13:19:29 -0500

    Small tweaks fix side nav z-index

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -4,11 +4,12 @@
 $(".button-collapse").sideNav();
 
 $(document).ready(function(){
-
+  
   var contactForm = document.querySelector('#contact form')
     contactForm.addEventListener('saved', function(e){
       contactForm.reset();
     })
+
 
   // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
   $('.modal').modal();
